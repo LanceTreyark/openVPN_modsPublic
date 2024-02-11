f5c4dc43522647461163a5c3d6a4771680410ad1
LanceTreyark <lance@treyark.com>
2024-02-11 11:34:55 -0800

    first commit

---
+++ Q_Com.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#v.020523
+#git add *
+date_time="$(date +"%m.%d.%y %I:%M%p")"
+read -p "Enter an additional commit message (optional)   " var_commit
+echo "*"
+git commit -m "$date_time $var_commit"
+echo "*  *"
+echo "*  *  *"
+sleep 1
+echo "Script v.020523 Complete"
+
